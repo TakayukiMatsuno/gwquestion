a3b702e661ae2293f3ef7959776b799f24d01096
naro143 <y.ishimi@itdharman.com>
2019-04-30 21:54:36 +0900

    fix: Q6

--- Q6.py
+++ Q6.py
@@ -1,39 +1,57 @@
 import random
-distance=5
-mhp=20
-while distance>1:
-    print("---------------------------------\n出口まで"+str(distance)+"歩\nHP"+str(mhp))
-    print("---------------------------------\n1歩進んだ\n---------------------------------")
-    battle="敵が現れた！"
-    cure="HPが3回復した！"
-    nothing="何もおきなかった"
-    event=[battle,cure,nothing]
-    happening=random.choice(event)
+
+distance = 5
+player_hp = 20
+while distance > 1:
+    print("---------------------------------")
+    print("出口まで{0}歩".format(distance))
+    print("HP: {0}".format(player_hp))
+    print("---------------------------------")
+    print("1歩進んだ")
+    print("---------------------------------")
+    battle = "敵が現れた！"
+    cure = "HPが3回復した！"
+    nothing = "何もおきなかった"
+    event = [battle, cure, nothing]
+    happening = random.choice(event)
     print(happening)
-    if happening==battle:
-        ehp=5
-        while ehp>0:
-            print("---------------------------------\n敵のHP"+str(ehp)+"\n自分のHP"+str(mhp))
-            print("---------------------------------\nコマンドを選択\n1:たたかう\n2:にげる")
-            choice=input()
-            if choice=="1":
-                print("---------------------------------\nプレーヤーの攻撃！\n4のダメージ\n敵の攻撃！\n3のダメージ")
-                ehp=ehp-4
-                mhp=mhp-3
-            elif choice=="2":
-                l=["ok","failure"]
-                away=random.choice(l)
-                if away=="ok":
+    if happening == battle:
+        enemy_hp = 5
+        while enemy_hp > 0:
+            print("---------------------------------")
+            print("敵のHP: {0}".format(enemy_hp))
+            print("自分のHP: {0}".format(player_hp))
+            print("---------------------------------")
+            print("コマンドを選択")
+            print("1:たたかう")
+            print("2:にげる")
+            choice = input()
+            if choice == "1":
+                print("---------------------------------")
+                print("プレーヤーの攻撃！")
+                print("4のダメージ")
+                print("敵の攻撃！")
+                print("3のダメージ")
+                enemy_hp = enemy_hp - 4
+                player_hp = player_hp - 3
+            elif choice == "2":
+                escape_result = ["ok", "failure"]
+                away = random.choice(escape_result)
+                if away == "ok":
                     print("逃げることに成功した！")
                     break
-                elif away=="failure":
+                elif away == "failure":
                     print("まわりこまれた！")
-        else: 
+        else:
             print("敵を倒した！")
-    elif happening==cure:
-        mhp=mhp+3
-    distance=distance-1
+    elif happening == cure:
+        player_hp = player_hp + 3
+    distance = distance - 1
 else:
-    print("---------------------------------\n出口まで"+str(distance)+"歩\nHP"+str(mhp))
-    print("---------------------------------\n1歩進んだ\n---------------------------------")
+    print("---------------------------------")
+    print("出口まで{0}歩".format(distance))
+    print("HP: {0}".format(player_hp))
+    print("---------------------------------")
+    print("1歩進んだ")
+    print("---------------------------------")
     print("おめでとう！洞窟を出られました！")
