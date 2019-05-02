552ff69b16176c43ed55ad1611ca5a1ce9fc1ee3
naro143 <y.ishimi@itdharman.com>
2019-05-02 09:12:15 +0900

    fix: Q4

--- Q4.py
+++ Q4.py
@@ -3,14 +3,17 @@ print("買い物の合計金額を入力してください。")
 cost = int(input())
 print("支払う金額を入力してください。")
 pay = int(input())
-change = cost - pay
-print("お釣りは、{0}円で、".format(change))
-for money in MONEY_KIND_LIST:
-    unit = ""
-    if money > 500:
-        unit = "札"
-    else:
-        unit = "玉"
-    print("{0}円{1}が{2}枚".format(money, unit, change // money))
-    change -= change // money * money
-print("です。")
+if pay < cost:
+    print("金額が足りません。")
+else:
+    change = cost - pay
+    print("お釣りは、{0}円で、".format(change))
+    for money in MONEY_KIND_LIST:
+        unit = ""
+        if money > 500:
+            unit = "札"
+        else:
+            unit = "玉"
+        print("{0}円{1}が{2}枚".format(money, unit, change // money))
+        change -= change // money * money
+    print("です。")
--- Q4_use_list.py
+++ Q4_use_list.py
@@ -4,17 +4,20 @@ print("買い物の合計金額を入力してください。")
 cost = int(input())
 print("支払う金額を入力してください。")
 pay = int(input())
-change = cost - pay
-for money in MONEY_KIND_LIST:
-    money_count_list.append(change // money)
-    change -= change // money * money
+if pay < cost:
+    print("金額が足りません。")
+else:
+    change = cost - pay
+    for money in MONEY_KIND_LIST:
+        money_count_list.append(change // money)
+        change -= change // money * money
 
-print("お釣りは、{0}円で、".format(cost - pay))
-print("10000円札が{0}枚".format(money_count_list[0]))
-print("5000円札が{0}枚".format(money_count_list[1]))
-print("1000円札が{0}枚".format(money_count_list[2]))
-print("500円玉が{0}枚".format(money_count_list[3]))
-print("100円玉が{0}枚".format(money_count_list[4]))
-print("50円玉が{0}枚".format(money_count_list[5]))
-print("10円玉が{0}枚".format(money_count_list[6]))
-print("5円玉が{0}枚".format(money_count_list[7]))
+    print("お釣りは、{0}円で、".format(cost - pay))
+    print("10000円札が{0}枚".format(money_count_list[0]))
+    print("5000円札が{0}枚".format(money_count_list[1]))
+    print("1000円札が{0}枚".format(money_count_list[2]))
+    print("500円玉が{0}枚".format(money_count_list[3]))
+    print("100円玉が{0}枚".format(money_count_list[4]))
+    print("50円玉が{0}枚".format(money_count_list[5]))
+    print("10円玉が{0}枚".format(money_count_list[6]))
+    print("5円玉が{0}枚".format(money_count_list[7]))
